121d27ac90cd6d7c1b1f5adfd5dcbc4531b98579
Carl Sundvisson <carl@jeesoft.uk>
2021-04-14 07:51:25 +0200

    Module7-Demo2 added

---
+++ Edureka/Demos/Module7-Demo2/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveComponent } from './reactive/reactive.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactiveComponent    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
