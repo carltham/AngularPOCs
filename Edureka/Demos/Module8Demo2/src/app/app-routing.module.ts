966da7e7f12e47361e1786782c7127c8f18233f5
Carl Sundvisson <carl@jeesoft.uk>
2021-04-18 18:29:28 +0200

    Refactoring, cleanup

--- Edureka/Demos/Module8Demo2/src/app/app-routing.module.ts
+++ Edureka/Demos/Module8Demo2/src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component'
-import { AuthGuard } from './_helpers/auth.guard'
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
+import { AuthGuard } from "./_helpers/auth.guard";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: "", component: HomeComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
 
-  { path: '**', redirectTo: '' }
+  { path: "**", redirectTo: "" },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Edureka/Demos/Module8Demo2/src/app/app.module.ts
+++ Edureka/Demos/Module8Demo2/src/app/app.module.ts
@@ -1,19 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-
-import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
-import { fakeBackendProvider } from './_helpers/fake-backend';
-import { AlertComponent } from './alert/alert.component'
-
-import { JwtInterceptor } from './_helpers/jwt.interceptor';
-import { ErrorInterceptor } from './_helpers/error.interceptor'
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AlertComponent } from "./alert/alert.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { ErrorInterceptor } from "./_helpers/error.interceptor";
+import { fakeBackendProvider } from "./_helpers/fake-backend";
+import { JwtInterceptor } from "./_helpers/jwt.interceptor";
 
 @NgModule({
   declarations: [
@@ -21,18 +18,19 @@ import { ErrorInterceptor } from './_helpers/error.interceptor'
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    AlertComponent
+    AlertComponent,
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    fakeBackendProvider],
-  bootstrap: [AppComponent]
+    fakeBackendProvider,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
