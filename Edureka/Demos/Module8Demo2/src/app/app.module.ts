cc501f7854f747a7d06af628b34322038f527c06
Carl Sundvisson <carl@jeesoft.uk>
2021-04-19 18:05:46 +0200

    Adjusting demo project Module8Demo2

--- Edureka/Demos/Module8Demo2/src/app/app.module.ts
+++ Edureka/Demos/Module8Demo2/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AlertComponent } from "./alert/alert.component";
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from "./routing/app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
