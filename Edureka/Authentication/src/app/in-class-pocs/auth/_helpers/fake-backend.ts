0429979e4a88a715d1a3852b1141804c2d86f062
Carl Sundvisson <carl@jeesoft.uk>
2021-04-19 18:24:50 +0200

    Module8Demo2 incorporated and working

--- Edureka/Authentication/src/app/in-class-pocs/auth/_helpers/fake-backend.ts
+++ Edureka/Authentication/src/app/in-class-pocs/auth/_helpers/fake-backend.ts
@@ -1,16 +1,16 @@
-import { Injectable } from "@angular/core";
 import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
   HttpRequest,
   HttpResponse,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
   HTTP_INTERCEPTORS,
 } from "@angular/common/http";
-import { Observable, of, throwError } from "rxjs";
-import { delay, mergeMap, materialize, dematerialize } from "rxjs/operators";
-import { User } from "../_models/user";
+import { Injectable } from "@angular/core";
 import * as moment from "moment";
+import { Observable, of, throwError } from "rxjs";
+import { delay, dematerialize, materialize, mergeMap } from "rxjs/operators";
+import { User } from "../_models/user";
 
 // array in local storage for registered users
 let usersString = localStorage.getItem("users");
@@ -60,42 +60,6 @@ export class FakeBackendInterceptor implements HttpInterceptor {
       }
     }
 
-    // route functions
-
-    function register() {
-      const user = body;
-
-      if (users.find((x: User) => x.username === user.username)) {
-        return error('Username "' + user.username + '" is already taken');
-      }
-
-      user.id = users.length
-        ? Math.max(...users.map((x: User) => x.id)) + 1
-        : 1;
-      users.push(user);
-      localStorage.setItem("users", JSON.stringify(users));
-
-      return ok();
-    }
-
-    function authenticate() {
-      const { username, password } = body;
-      const user = users.find(
-        (x: User) => x.username === username && x.password === password
-      );
-      if (!user) {
-        return error("Username or password is incorrect");
-      }
-
-      return ok({
-        id: user.id,
-        username: user.username,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        token: "fake-jwt-token",
-      });
-    }
-
     function getUsers() {
       if (!isLoggedIn()) return unauthorized();
       return ok(users);
@@ -108,21 +72,6 @@ export class FakeBackendInterceptor implements HttpInterceptor {
       localStorage.setItem("users", JSON.stringify(users));
       return ok();
     }
-
-    // helper functions
-
-    function ok(body: any = "") {
-      return of(new HttpResponse({ status: 200, body }));
-    }
-
-    function error(message: string) {
-      return throwError({ error: { message } });
-    }
-
-    function unauthorized() {
-      return throwError({ status: 401, error: { message: "Unauthorised" } });
-    }
-
     function isLoggedIn() {
       console.log("FakeBackendInterceptor::isLoggedIn:moment() = ", moment());
       console.log(
@@ -151,6 +100,60 @@ export class FakeBackendInterceptor implements HttpInterceptor {
       console.log("expiresMoment = ", expiresMoment);
       console.log("id_token = ", id_token);
       return moment(expiresAt);
+    }
+
+    // route functions
+
+    function authenticate() {
+      const { username, password } = body;
+      const user = users.find(
+        (x: User) => x.username === username && x.password === password
+      );
+      if (!user) {
+        return error("Username or password is incorrect");
+      }
+
+      return ok({
+        id: user.id,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        token: "fake-jwt-token",
+      });
+    }
+
+    function register() {
+      const requestedUser = body;
+
+      if (
+        users.find((user: User) => user.username === requestedUser.username)
+      ) {
+        return error(
+          'Username "' + requestedUser.username + '" is already taken'
+        );
+      }
+
+      requestedUser.id = users.length
+        ? Math.max(...users.map((x: User) => x.id)) + 1
+        : 1;
+      users.push(requestedUser);
+      localStorage.setItem("users", JSON.stringify(users));
+
+      return ok();
+    }
+
+    // helper functions
+
+    function ok(body: any = "") {
+      return of(new HttpResponse({ status: 200, body }));
+    }
+
+    function error(message: string) {
+      return throwError({ error: { message } });
+    }
+
+    function unauthorized() {
+      return throwError({ status: 401, error: { message: "Unauthorised" } });
     }
 
     function idFromUrl() {
